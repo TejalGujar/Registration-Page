ab200a6253128d7870302ea19dd7721e038202c1
Tejal <tejbadgujar22@gmail.com>
2021-04-05 19:35:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/registrationpage/LoginActivity.java
@@ -0,0 +1,69 @@
+package com.example.registrationpage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity
+{
+
+    EditText edtEmail,edtPass;
+    Button btnLogin;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity_layout);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPass = findViewById(R.id.edtPass);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser(edtEmail.getText().toString(),edtPass.getText().toString());
+            }
+        });
+    }
+
+    private  void loginUser(String email,String pass){
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_LONG).show();
+
+                    Intent homeIntent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                    startActivity(homeIntent);
+
+                }else{
+                    Toast.makeText(LoginActivity.this,"Login Error",Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this,"Login Fail",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
